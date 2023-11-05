de47dcf9cde8816af24bed234a6d9a4ddfefbfa6
jorgensd <dokken92@gmail.com>
2023-11-05 14:01:37 +0100

    Add more to tutorial

--- src/code_generation.py
+++ src/code_generation.py
@@ -1,30 +1,7 @@
 
 # # Code generation
-# All the code above is Python code symbolically describing the variational form of the projection.
+# All the code in the previous section is Python code symbolically describing the variational form of the projection.
 
-# To do so, we would define each of the functions as the linear
-# combinations of the basis functions
-# $u=\sum_{i=0}^{\mathcal{N}}u_i\phi_i(x)\qquad
-# v=\sum_{i=0}^{\mathcal{N}}v_i\phi_i(x)\qquad
-# f=\sum_{k=0}^{\mathcal{M}}f_k\psi_k(x)\qquad
-# g=\sum_{l=0}^{\mathcal{T}}g_l\varphi_l(x)$
-#
-# We next use the map $M_K:K_{ref}\mapsto K$
-# \begin{align}
-# \int_\Omega u v~\mathrm{d}x&= \sum_{K\in\mathcal{K}}\int_K u(x) v(x)~\mathrm{d}x\\
-# &= \sum_{K\in\mathcal{K}}\int_{M_K(K_{ref})} u(x)v(x)~\mathrm{d}x\\
-# &= \sum_{K\in\mathcal{K}}\int_{K_{ref}}u(M_K(\bar x))v(M_K(\bar x))\vert \mathrm{det} J_K(\bar x)\vert~\mathrm{d}\bar x
-# \end{align}
-# where $K$ is each element in the physical space, $J_K$ the Jacobian of the mapping.
-# Next, we can insert the expansion of $u$ into the formulation and identify the matrix system $Au=b$, where
-# \begin{align}
-# A_{j, i} &= \int_{K_{ref}} \phi_i(M_K(\bar x))\phi_j(M_K(\bar x))\vert \mathrm{det} J_K(\bar x)\vert~\mathrm{d}\bar x\\
-# b_j &= \int_{K_{ref}} \frac{\Big(\sum_{k=0}^{\mathcal{M}}f_k\psi_i(M_K(\bar x))\Big)}
-# {\Big(\sum_{l=0}^{\mathcal{T}}g_k\varphi_i(M_K(\bar x))\Big)}\phi_j(M_K(\bar x))\vert \mathrm{det} J_K(\bar x)\vert~\mathrm{d}\bar x
-# \end{align}
-# Next, one can choose an appropriate quadrature rule with points and weights, include the
-# correct mapping/restrictions of degrees of freedom for each cell.
-# All of this becomes quite tedious and error prone work, and has to be repeated for every variational form!
 # This is why we in FEniCS rely on code generation.
 # One can interpret the variational form written in UFL as a directed acyclic graph of operations,
 # where we for each simple operation can implement it as C code.
@@ -37,6 +14,7 @@
 
 import ffcx.main
 from pathlib import Path
+import os
 cwd = Path.cwd()
 infile = cwd / "introduction.py"
 
@@ -47,3 +25,11 @@ ffcx.main.main(["-o", str(cwd), "--visualise", str(infile)])
 # ![Bilinear graph](S_a.png)
 # ## Linear graph
 # ![Linear graph](S_L.png)
+
+# With this graph, we also get compiled `c` code:
+
+os.system("ls introduction.*")
+
+# We can look at the assembly code for the local matrix
+
+os.system("head -1191 introduction.c | tail +1142")
--- src/introduction.py
+++ src/introduction.py
@@ -126,8 +126,51 @@ a = ufl.inner(u, v) * ufl.dx
 # Note that the bi-linear form `a` is defined with trial and test functions.
 
 L = (f / g) * v * ufl.dx
-
+forms = [a, L]
 # So far, so good?
 # As opposed to most demos/tutorials on FEniCSx, note that we have not imported `dolfinx` or made a reference to the actual
 # computational domain we want to solve the problem on or what `f` or `g` is,
 # except for the choice of function spaces
+
+
+# ## Further analysis of the variational form
+# To do so, we would define each of the functions as the linear
+# combinations of the basis functions
+# $u=\sum_{i=0}^{\mathcal{N}}u_i\phi_i(x)\qquad
+# v=\sum_{i=0}^{\mathcal{N}}v_i\phi_i(x)\qquad
+# f=\sum_{k=0}^{\mathcal{M}}f_k\psi_k(x)\qquad
+# g=\sum_{l=0}^{\mathcal{T}}g_l\varphi_l(x)$
+#
+# We next use the map $M_K:K_{ref}\mapsto K$
+# \begin{align}
+# \int_\Omega u v~\mathrm{d}x&= \sum_{K\in\mathcal{K}}\int_K u(x) v(x)~\mathrm{d}x\\
+# &= \sum_{K\in\mathcal{K}}\int_{M_K(K_{ref})} u(x)v(x)~\mathrm{d}x\\
+# &= \sum_{K\in\mathcal{K}}\int_{K_{ref}}u(M_K(\bar x))v(M_K(\bar x))\vert \mathrm{det} J_K(\bar x)\vert~\mathrm{d}\bar x
+# \end{align}
+# where $K$ is each element in the physical space, $J_K$ the Jacobian of the mapping.
+# Next, we can insert the expansion of $u$ into the formulation and identify the matrix system $Au=b$, where
+# \begin{align}
+# A_{j, i} &= \int_{K_{ref}} \phi_i(M_K(\bar x))\phi_j(M_K(\bar x))\vert \mathrm{det} J_K(\bar x)\vert~\mathrm{d}\bar x\\
+# b_j &= \int_{K_{ref}} \frac{\Big(\sum_{k=0}^{\mathcal{M}}f_k\psi_i(M_K(\bar x))\Big)}
+# {\Big(\sum_{l=0}^{\mathcal{T}}g_k\varphi_i(M_K(\bar x))\Big)}\phi_j(M_K(\bar x))\vert \mathrm{det} J_K(\bar x)\vert~\mathrm{d}\bar x
+# \end{align}
+# Next, one can choose an appropriate quadrature rule with points and weights, include the
+# correct mapping/restrictions of degrees of freedom for each cell.
+# All of this becomes quite tedious and error prone work, and has to be repeated for every variational form!
+
+pulled_back_L = ufl.algorithms.compute_form_data(L,
+                                                 do_apply_function_pullbacks=True,
+                                                 do_apply_integral_scaling=True,
+                                                 do_apply_geometry_lowering=True,
+                                                 preserve_geometry_types=(
+                                                     ufl.classes.Jacobian,))
+print(pulled_back_L.integral_data[0])
+
+# We can also expand the computation of the Jacobian
+#
+
+expanded_L = ufl.algorithms.compute_form_data(L,
+                                              do_apply_function_pullbacks=True,
+                                              do_apply_integral_scaling=True,
+                                              do_apply_geometry_lowering=True)
+print(expanded_L.integral_data[0])
