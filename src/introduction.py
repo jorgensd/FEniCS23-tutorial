e1bb13876e19cc23a6c7f695019369e204465aa0
Jørgen S. Dokken <dokken@simula.no>
2024-08-23 15:50:08 +0200

    Start reformatting and rephrasing tutorial

--- src/introduction.py
+++ src/introduction.py
@@ -9,44 +9,51 @@
 # \begin{align}
 # u &= \frac{f(x,y,z)}{g(x,y,z)} \qquad \text{in } \Omega\subset \mathbb{R}^3.
 # \end{align}
-# where $\Omega$ is our computational domain, $f$ and $g$ are two known functions
+# where $\Omega$ is our computational domain, $f$ and $g$ are two known functions.
 #
 # + [markdown] slideshow={"slide_type": "slide"}
 # ## Finite elements
 # + [markdown] slideshow={"slide_type": "skip"}
 # To solve this problem, we have to choose an appropriate finite element space to represent the function $k$ and $u$.
-# There is a large variety of finite elements, for instance the [Lagrange elements](https://defelement.com/elements/lagrange.html).
+# There is a large variety of finite elements, for instance the
+# [Lagrange elements](https://defelement.com/elements/lagrange.html).
 # The basis function for a first order Lagrange element is shown below
 #
-#<center> 
-# <img src="https://defelement.com/img/element-Lagrange-variant-equispaced-triangle-1-0-large.png" width="150" height="150" />
-# <img src="https://defelement.com/img/element-Lagrange-variant-equispaced-triangle-1-1-large.png" width="150" height="150" />
-# <img src="https://defelement.com/img/element-Lagrange-variant-equispaced-triangle-1-2-large.png" width="150" height="150" /><br>
+# <center>
+# <img src="https://defelement.com/img/element-Lagrange-variant-equispaced-triangle-1-0-large.png"
+# width="150" height="150" />
+# <img src="https://defelement.com/img/element-Lagrange-variant-equispaced-triangle-1-1-large.png"
+# width="150" height="150" />
+# <img src="https://defelement.com/img/element-Lagrange-variant-equispaced-triangle-1-2-large.png"
+# width="150" height="150" /><br>
 # First order Lagrange basis functions<br><br>
 # </center>
 
 #
-# To symbolically represent this element, we use `basix.ufl.element`, that creates a UFL-compatible element using Basix.
+# To symbolically represent this element, we use `basix.ufl.element`,
+# that creates a UFL-compatible element using [Basix](https://github.com/FEniCS/basix/).
 
 # + slideshow={"slide_type": ""}
 import numpy as np
+
 import basix.ufl
 
 element = basix.ufl.element("Lagrange", "triangle", 1)
 
 # -
-# We note that we send in the finite element family, what cells we will use to represent the domain,
-# and the degree of the function space.
-# As this element is just a symbolic representation, it does not contain the information required to tabulate (evaluate)
-# basis functions at an arbitrary point. UFL is written in this way, so that it can be used as a symbolic representation
-# for a large range of finite element software, not just FENiCS.
+# The three arguments we provide to Basix are the name of the element family, the reference
+# cell we would like to use and the degree of the polynomial space.
 #
 
-# Lets next tabulate the basis functions of our element at a given point `p=(0, 0.5)` in the reference element.
-# the `basix.finite_element.FiniteElement.tabulate` function takes in two arguments, how many derivatives we want to compute,
-# and at what points in the reference element we want to compute them.
+# Next, we will evaluate the basis functions at a certain set of `points` in the reference triangle.
+# We call this tabulation and we use the `tabulate` method of the element object.
+# The two input arguments are:
+# 1) The number of spatial derivatives of the basis functions we want to compute.
+# 2) A set of input points to evaluate the basis functions at as a numpy array of shape
+# # `(num_points, reference_cell_dimension)`.
+# In this case, we want to compute the basis functions themselves, so we set the first argument to 0.
 
-points = np.array([[0., 0.5], [1, 0]], dtype=np.float64)
+points = np.array([[0.0, 0.5], [1, 0]], dtype=np.float64)
 print(element.tabulate(0, points))
 
 # We can also compute the derivatives at any points in the reference element
@@ -56,11 +63,13 @@ print(element.tabulate(1, points))
 # Observe that the output we get from this command also includes the 0th order derivatives.
 # Thus we note that the output has the shape `(num_spatial_derivatives+1, num_points, num_basis_functions)`
 #
-# Not every function we want to represent is scalar valued. For instance, in fluid flow problems, the Taylor-Hood finite element
-# pair is often used to represent the fluid velocity and pressure, where each component of the fluid velocity is in a Lagrange space.
+# Not every function we want to represent is scalar valued.
+# For instance, in fluid flow problems, the Taylor-Hood finite element pair is often used
+# to represent the fluid velocity and pressure.
+# For the velocity, each component (x, y, z) is represented with its own degrees of freedom in a Lagrange space..
 # We represent this by adding a `shape` argument to the `basix.ufl.element` constructor.
 
-vector_element = basix.ufl.element("Lagrange", "triangle", 2, shape=(2, ))
+vector_element = basix.ufl.element("Lagrange", "triangle", 2, shape=(2,))
 
 # Basix allows for a large variety of extra options to tweak your finite elements, see for instance
 # [Variants of Lagrange elements](https://docs.fenicsproject.org/dolfinx/v0.8.0/python/demos/demo_lagrange_variants.html)
@@ -72,3 +81,15 @@ m_el = basix.ufl.mixed_element([vector_element, element])
 
 # There is a wide range of finite elements that are supported by ufl and basix.
 # See for instance: [Supported elements in basix/ufl](https://defelement.com/lists/implementations/basix.ufl.html).
+
+# ## Lower precision tabulation
+#
+# In some cases, one might want to use a lower accuracy for tabulation of basis functions to speed up computations.
+# This can be changed in basix by adding `dtype=np.float32` to the element constructor.
+
+low_precision_element = basix.ufl.element("Lagrange", "triangle", 1, dtype=np.float32)
+points_low_precision = points.astype(np.float32)
+basis_values = low_precision_element.tabulate(0, points_low_precision)
+print(f"{basis_values=}\n   {basis_values.dtype=}")
+
+# We observe that elements that are close to zero is now an order of magnitude larger than its `np.float64` counterpart.
